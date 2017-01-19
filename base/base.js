254ad7880d85f6be803e3123242bb47182941829
Kay Sackey <kay@9cloud.us>
2017-01-19 11:49:33 -0500

    Fix imports to work with Rollup

--- base/base.js
+++ base/base.js
@@ -1,6 +1,6 @@
 import React, {Component as ReactComponent} from "react";
 import {computed} from "mobx";
-import {observer} from "mobx-react";
+import {default as mobxReact} from "mobx-react";
 import {use, bind_all_react_component_methods} from "tide/utils";
 import {BasicEventHandler} from "tide/base/events";
 
@@ -17,4 +17,4 @@ import {BasicEventHandler} from "tide/base/events";
  * @class MobxObserver
  * @extends ReactComponent
  */
-export const MobxObserver = observer(ReactComponent);
+export const MobxObserver = mobxReact.observer(ReactComponent);
